e40c76cf84a103066976dab9e11195c1711a39a8
Alex Biedny <abiedny09@gmail.com>
2019-10-27 20:54:10 -0500

    ugh

--- Discussion 7/EE3015_Discussion7.m
+++ Discussion 7/EE3015_Discussion7.m
@@ -1,22 +1,42 @@
-load('3015_Discussion_7_Matlab.mat')
-t = (0:1/fs:(length(oboe)-1)/fs)';
-plot(t, oboe); %full signal plot
+load('3015_Discussion_7_Matlab.mat');
 
-start_sustain_index = find(t == 0.5);
-end_sustain_index = find(t == 1);
-oboe_sustain = oboe(start_sustain_index:end_sustain_index);
-t_sustain = t(start_sustain_index:end_sustain_index);
-hold on;
-plot(t_sustain, oboe_sustain); %just a chunk of the sustain portion, for reference
-
-starti = find(oboe_sustain == 0, 1);
-[accuracy, endi] = min(abs(t_sustain - (t_sustain(starti) + 1/440)));
-period = oboe_sustain(starti:endi);
-t_period = t_sustain(starti:endi);
-hold off; %clear out the other plots to plot one period
-figure(1);
-plot(t_period, period); % one period plot
-figure(2);
-coeffs = fft(period)/length(period);
-coeffs = fftshift(coeffs);
-stem(abs(coeffs));
+for n = 1:4
+    switch n
+        case 1
+            signal = oboe;
+            w0 = 440;
+        case 2
+            signal = sax;
+            w0 = 440;
+        case 3
+            t_cosine = 0:1/fs:5;
+            cosine=cos(2*pi*t_cosine*440);
+            signal = cosine;
+            w0 = 440;
+        case 4
+            signal = piano;
+            w0 = 220;
+    end
+    
+    t = (0:1/fs:(length(signal)-1)/fs)';
+    plot(t, signal); %full signal plot
+    
+    start_sustain_index = find(t == 0.5);
+    end_sustain_index = find(t == 1);
+    signal_sustain = signal(start_sustain_index:end_sustain_index);
+    t_sustain = t(start_sustain_index:end_sustain_index);
+    hold on;
+    plot(t_sustain, signal_sustain); %just a chunk of the sustain portion, for reference
+    
+    starti = find(signal_sustain == 0, 1);
+    [accuracy, endi] = min(abs(t_sustain - (t_sustain(starti) + 1/w0)));
+    period = signal_sustain(starti:endi);
+    t_period = t_sustain(starti:endi);
+    hold off; %clear out the other plots to plot one period
+    figure(n);
+    plot(t_period, period); % one period plot
+    figure(n+4);
+    coeffs = fft(period)/length(period);
+    coeffs = fftshift(coeffs);
+    stem(abs(coeffs));
+end
